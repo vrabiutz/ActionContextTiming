d9f34c90f1afed1fca39bcb3ef2a6431a1a00bf6
PC1022250256\LocalAdmin <gregor.volberg@gmail.com>
2023-02-09 21:05:37 +0100

    purr

--- read_accuracies.R
+++ read_accuracies.R
@@ -14,9 +14,23 @@ accuracies <- read.csv('./data/accuracies_raw.csv') %>%
 
 # normalization? per picture, dimension and participant
 #Fei-Fei (2007) page 8: The scores were then normalized: The seven scores for a given image (one for each PT) were divided by the highest score achieved for that image (across all PTs). All evaluation scores were therefore between 0 and 1. Due to this within-image normalization, inherent differences in difficulty of perceiving or understanding scenes between different images were eliminated. [PT = presentation time]
-l=accuracies %>% group_by(participant, fname) %>%
+accuracies %>% group_by(participant, fname) %>%
                group_split()%>%
                map(~ .$Action / max(.$Action))
+               
+accuracies %>% group_by(participant, fname) %>%
+  group_split()%>%
+  map(~ .$Action / max(.$Action)) %>%
+  simplify()
+
+https://dcl-prog.stanford.edu/purrr-mutate.html
+
+llist = accuracies %>% group_by(participant, fname) %>%
+  group_split()
+
+l %>% mutate(across(Action:Object),
+             )
+  map(~ .$Action / max(.$Action))
 
 accuracies %>% mutate(across(Action:Object,
                              group_by(participant, fname) %>%
