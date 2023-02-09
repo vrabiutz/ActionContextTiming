311f6b816582dd46fba5c836cec46b77dfd2a9e4
Gregor Volberg <gregor.volberg@ur.de>
2023-02-09 16:36:25 +0100

    added data and routine for data reading

---
+++ read_accuracies.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(ggplot2)
+
+#counts     <- read.csv('./data/counts.csv') 
+# as yet: 10 different pictures with 6 different presentation times each = 60 rows
+
+accuracies <- read.csv('./data/accuracies_raw.csv') %>%
+    mutate(trialNum = 1:nrow(.),
+           participant  = 'S001') %>%
+    arrange(accuracies, category, prestime) %>%
+    select(8:9, 1:7) %>%
+    as_tibble()
+
+
+# normalization? per picture, dimension and participant
+#Fei-Fei (2007) page 8: The scores were then normalized: The seven scores for a given image (one for each PT) were divided by the highest score achieved for that image (across all PTs). All evaluation scores were therefore between 0 and 1. Due to this within-image normalization, inherent differences in difficulty of perceiving or understanding scenes between different images were eliminated. [PT = presentation time]
+l=accuracies %>% group_by(participant, fname) %>%
+               group_split()%>%
+               map(~ .$Action / max(.$Action))
+
+accuracies %>% mutate(across(Action:Object,
+                             group_by(participant, fname) %>%
+                               group_split()%>%
+                               map(~ .x / max(.x))))
+             
