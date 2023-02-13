1e74d6a8aeca51ec7bfcc9bac1414863f408239b
Gregor Volberg <gregor.volberg@ur.de>
2023-02-13 12:26:22 +0100

    with plots

--- read_accuracies.R
+++ read_accuracies.R
@@ -1,15 +1,12 @@
 library(tidyverse)
-library(ggplot2)
+#library(ggplot2)
 library(quickpsy)
-
-# data is 10 different pictures with 6 different presentation times each = 60 rows
 
 counts <- read.csv('./data/counts.csv') %>%
   as_tibble() %>%
   mutate(trialNum = 1:nrow(.),
          id       = 'S001') %>%
   select(trialNum:id, category, fname, prestime, ends_with('_correct')) %>%
-  #arrange(id, category, fname, prestime) %>%
   rename(Action = Action_correct,
          Context = Context_correct,
          Object = Object_correct,
@@ -23,7 +20,8 @@ counts <- read.csv('./data/counts.csv') %>%
   ungroup() %>%
   mutate(across(id:fname,  as.factor)) %>%
   arrange(id, Target, category, fname, prestime)
-  
+
+# descriptives  
 tabC <- counts %>% 
   group_by(id, Target, prestime) %>%
   summarize(k = sum(k),
@@ -32,73 +30,85 @@ tabC <- counts %>%
   ungroup()
 
 
-# read data and normalize accuracies per participant, picture and dimension
-accuracies <- read.csv('./data/accuracies_raw.csv') %>%
-    as_tibble() %>%
-    mutate(trialNum = 1:nrow(.),
-           id       = 'S001') %>%
-    select(8:9, 1:7) %>%
-    arrange(id, category, fname, prestime) %>%
-    mutate(across(id:category,  as.factor)) %>%
-    group_by(id, fname) %>% # normalize
-    mutate(across(Action:Sensory, ~.x/max(.x), 
-                  .names = 'n_{col}')) %>%
-    ungroup()
+# # read data and normalize accuracies per participant, picture and dimension
+# accuracies <- read.csv('./data/accuracies_raw.csv') %>%
+#     as_tibble() %>%
+#     mutate(trialNum = 1:nrow(.),
+#            id       = 'S001') %>%
+#     select(8:9, 1:7) %>%
+#     arrange(id, category, fname, prestime) %>%
+#     mutate(across(id:category,  as.factor)) %>%
+#     group_by(id, fname) %>% # normalize
+#     mutate(across(Action:Sensory, ~.x/max(.x), 
+#                   .names = 'n_{col}')) %>%
+#     ungroup()
 
-tabA <- accuracies %>% 
-  group_by(id, prestime) %>%
-  summarize(Action = mean(n_Action),
-            Context = mean(n_Context),
-            Object = mean(n_Object),
-            Sensory = mean(n_Sensory)) %>%
-  ungroup()
+# tabA <- accuracies %>% 
+#   group_by(id, prestime) %>%
+#   summarize(Action = mean(n_Action),
+#             Context = mean(n_Context),
+#             Object = mean(n_Object),
+#             Sensory = mean(n_Sensory)) %>%
+#   ungroup()
 
-
-  
-  
 # fit without guesses and lapses
-fit1 <- counts %>% 
-        filter(Target != 'Sensory' & prestime != 500) %>%
-        quickpsy(., prestime, k, N_normalized, grouping = .(id, Target), 
-           bootstrap = 'none')  
-
-tabF <- fit1$averages %>%
-  group_by(id, Target, prestime) %>%
-  summarize(correct = mean(prob)) %>%
-  ungroup()
-
-  
-  
-    
+# fit1 <- counts %>% 
+#         filter(Target != 'Sensory' & prestime != 500) %>%
+#         quickpsy(., prestime, k, N_normalized, grouping = .(id, Target), 
+#            bootstrap = 'none')  
+# 
+# tabF <- fit1$averages %>%
+#   group_by(id, Target, prestime) %>%
+#   summarize(correct = mean(prob)) %>%
+#   ungroup()
 #plotAndTest(fit1)
 
 
-# fit with guess and lapses; restrict parameter space for fitting
-fit2 <- counts %>% 
-  filter(Target != 'Sensory' & prestime < 500) %>%
-  quickpsy(., prestime, k, N_normalized, grouping = .(id, Target), 
-           lapses = TRUE, 
-           guess = TRUE, xmax = 100,
-           parini = list(c(0, 100), c(0, 100),c(0, 1), c(0, 1)),
-           bootstrap = 'none')
+# fit M, SD and guesses 
+# fix lapse rate to (1 - avgerage accuracy in 500 ms condition).
+# fit only data between 33.3 to 100 ms 
+fitted2 = counts %>%
+    filter(Target !='Sensory') %>%
+    group_by(id, Target) %>%
+    mutate(lapse = 1-sum(k[prestime == 500])/sum(N_normalized[prestime == 500])) %>%
+    group_split %>%
+    map(~quickpsy(., prestime, k, N_normalized, 
+                    lapses = unique(.$lapse), 
+                    guess = TRUE, xmax = 100,
+                    parini = list(c(0, 100), c(0, 100), c(0, 1)),
+                    bootstrap = 'none'))
 
+# plotting functions
+lapsepnorm <- function(x, m, sd, guess = 0, lapse = 0){
+  guess + (1 - guess - lapse) * pnorm(x, m, sd)
+}
 
+plotfun = function(plotparameters, plotcolor){
+          curve(lapsepnorm(x, plotparameters$par$par[1], # M
+                 plotparameters$par$par[2], # SD
+                 plotparameters$par$par[3], # guess
+                 plotparameters$lapses),     # lapse
+                 col = plotcolor, lwd = 2, 
+                 xaxt = 'n', add=T)
+          prb <- aggregate(prob ~ prestime, FUN=mean, data=plotparameters$averages)
+          points(prb$prestime, prb$prob,
+                 col=plotcolor, cex=1.5, pch = 19)
+}
 
-fit2 <- counts %>% 
-        filter(Target == 'Action') %>%
-        quickpsy(., prestime, k, N_normalized, grouping = .(id, Target), 
-                 lapses = 1-tabC$correct[tabC$Target == 'Action' & tabC$prestime == 500], 
-                 guess = TRUE, xmax = 100,
-                 parini = list(c(0, 100), c(0, 100),c(0, 1)),
-                 bootstrap = 'none')
-plotAndTest(fit2)
+# actual plotting
+plot(NULL, xlim = c(0,180), ylim = c(0,1),
+     ylab = 'p (correct)', xlab = 'presentation time (ms)', xaxt = 'n')
+plotfun(fitted2[[1]], 'red')
+plotfun(fitted2[[2]], 'blue')
+plotfun(fitted2[[3]], 'black')
 
-# normalization per picture, dimension and participant
-accuracies <- accuracies %>% 
-    group_by(id, fname) %>%
-    mutate(across(Action:Sensory, ~.x/max(.x), 
-                  .names = 'n_{col}')) %>%
-    ungroup() %>%
-    mutate(across(id:prestime,  as.factor))
+# add ticks, labels etc
+xvalues <- round(sort(unique(counts$prestime[counts$prestime < 500])))
+axis(1, at = xvalues)
+legend(10,0.9,legend = c('Action', 'Context', 'Object'), 
+       lty = c('solid', 'solid', 'solid'),
+       lwd = c(2,2,2),
+       col = c('red', 'blue', 'black'), bty='n')
+abline(h = 0.5, lty = 'dashed')
 
 
