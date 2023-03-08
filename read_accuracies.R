254c969e6a83f094fb9d8354b2ec1da034cae58d
Gregor Volberg <gregor.volberg@ur.de>
2023-03-08 16:28:28 +0100

    Goodness of fit and randomization test for differences in thresholds

--- read_accuracies.R
+++ read_accuracies.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-#library(ggplot2)
 library(quickpsy)
 
 counts <- read.csv('./data/counts.csv') %>%
@@ -29,54 +28,98 @@ tabC <- counts %>%
   mutate(correct = k/N) %>%
   ungroup()
 
-
-# # read data and normalize accuracies per participant, picture and dimension
-# accuracies <- read.csv('./data/accuracies_raw.csv') %>%
-#     as_tibble() %>%
-#     mutate(trialNum = 1:nrow(.),
-#            id       = 'S001') %>%
-#     select(8:9, 1:7) %>%
-#     arrange(id, category, fname, prestime) %>%
-#     mutate(across(id:category,  as.factor)) %>%
-#     group_by(id, fname) %>% # normalize
-#     mutate(across(Action:Sensory, ~.x/max(.x), 
-#                   .names = 'n_{col}')) %>%
-#     ungroup()
-
-# tabA <- accuracies %>% 
-#   group_by(id, prestime) %>%
-#   summarize(Action = mean(n_Action),
-#             Context = mean(n_Context),
-#             Object = mean(n_Object),
-#             Sensory = mean(n_Sensory)) %>%
-#   ungroup()
-
-# fit without guesses and lapses
-# fit1 <- counts %>% 
-#         filter(Target != 'Sensory' & prestime != 500) %>%
-#         quickpsy(., prestime, k, N_normalized, grouping = .(id, Target), 
-#            bootstrap = 'none')  
-# 
-# tabF <- fit1$averages %>%
-#   group_by(id, Target, prestime) %>%
-#   summarize(correct = mean(prob)) %>%
-#   ungroup()
-#plotAndTest(fit1)
-
-
-# fit M, SD and guesses 
-# fix lapse rate to (1 - avgerage accuracy in 500 ms condition).
-# fit only data between 33.3 to 100 ms 
+#####====== Fit and extract results
+# fit M, SD and guesses; use only data between 33.3 to 100 ms 
+# fix lapse rate to (1 - avgerage accuracy in 500 ms condition)
 fitted2 = counts %>%
     filter(Target !='Sensory') %>%
-    group_by(id, Target) %>%
+    group_by(Target) %>% # omitted id here!!!
     mutate(lapse = 1-sum(k[prestime == 500])/sum(N_normalized[prestime == 500])) %>%
     group_split %>%
     map(~quickpsy(., prestime, k, N_normalized, 
                     lapses = unique(.$lapse), 
                     guess = TRUE, xmax = 100,
                     parini = list(c(0, 100), c(0, 100), c(0, 1)),
-                    bootstrap = 'none'))
+                    bootstrap = 'nonparametric', B = 1000))
+
+# show deviance and p
+fitted2 %>% map_df("deviance")
+
+
+# write empirical thresholds
+empThresh = fitted2 %>% 
+  map("thresholds") %>%
+  map_dbl("thre")
+
+names(empThresh) = counts %>%
+  filter(Target !='Sensory') %>%
+  group_by(Target) %>%
+  group_keys(.) %>%
+  pull("Target")
+
+####=== randomization: generate distribution of differences between conditions 
+nruns = 1000
+conditions = list(c('Action', 'Context'),
+                  c('Action', 'Object'),
+                  c('Context', 'Object'))
+
+# allocate vars
+permThresh = rep(list(tibble(Targ1 = numeric(nruns)/0,
+                        Targ2 = numeric(nruns)/0,
+                        Targ1minus2 = numeric(nruns)/0,
+                        Targ1Name = char('NULL'),
+                        Targ2Name = char('NULL'),
+                        empDiff  = numeric(nruns)/0)),length(conditions))
+pvals = numeric(length(empThresh))/0
+
+# loop
+for (con in 1:length(conditions)){
+  for (k in 1:nruns){
+  tmp = counts %>% 
+    filter(Target == conditions[[con]][1] | Target == conditions[[con]][2]) %>%
+    group_by (prestime)  %>%
+    mutate(Target = sample(Target)) %>% # permute within prestime
+    ungroup(.) %>%
+    group_by(Target) %>%
+    mutate(lapse = 1-sum(k[prestime == 500])/sum (N_normalized[prestime == 500])) 
+  
+  firstCondition  = group_keys(tmp)$Target[1]
+  secondCondition = group_keys(tmp)$Target[2]
+  
+  fit = tmp %>%
+    group_split %>%
+    map(~quickpsy(., prestime, k, N_normalized, 
+                  lapses = unique(.$lapse), 
+                  guess = TRUE, xmax = 100,
+                  parini = list(c(0, 100), c(0, 100), c(0, 1)),
+                  bootstrap = 'none'))
+  
+  permThresh[[con]][k, 1] = fit[[1]]$thresholds$thre
+  permThresh[[con]][k, 2] = fit[[2]]$thresholds$thre
+  permThresh[[con]][k, 3] = fit[[1]]$thresholds$thre - fit[[2]]$thresholds$thre
+  permThresh[[con]][k, 4] = firstCondition
+  permThresh[[con]][k, 5] = secondCondition
+  permThresh[[con]][k, 6] = empThresh[conditions[[con]][1]] -
+                            empThresh[conditions[[con]][2]]
+  }
+  pvals[con] = sum(permThresh[[con]]$Targ1minus2 > permThresh[[con]]$empDiff[1]) / nruns
+}
+
+####==== plot permutation distribution and empirical data
+plotPerm <- function(permResults, pvals, comparison){
+  plot(ecdf(permResults[[comparison]]$Targ1minus2), 
+       main = paste(permResults[[comparison]]$Targ1Name[1], 
+                    '-', 
+                    permResults[[comparison]]$Targ2Name[1]), 
+          xlab = "Difference of detection thresholds")
+  points(permResults[[comparison]]$empDiff[1],
+         1-pvals[comparison],
+         col = 'red')
+}
+plotPerm(permThresh, pvals, 1)
+plotPerm(permThresh, pvals, 2)
+plotPerm(permThresh, pvals, 3)
+
 
 # plotting functions
 lapsepnorm <- function(x, m, sd, guess = 0, lapse = 0){
