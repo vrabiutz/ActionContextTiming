31a52a20c0f8b860977c8c710a72e731539677fc
Gregor Volberg <gregor.volberg@ur.de>
2023-03-09 14:45:54 +0100

    re-ordered code sections, added random seed for reproducable results, added upper/lower/two-sided p

--- read_accuracies.R
+++ read_accuracies.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(quickpsy)
 
-counts <- read.csv('./data/counts.csv') %>%
+rawDataFile <- './data/counts_r02.csv' 
+# './data/counts.csv'
+counts <- read.csv(rawDataFile) %>%
   as_tibble() %>%
   mutate(trialNum = 1:nrow(.),
          id       = 'S001') %>%
@@ -45,7 +47,6 @@ fitted2 = counts %>%
 # show deviance and p
 fitted2 %>% map_df("deviance")
 
-
 # write empirical thresholds
 empThresh = fitted2 %>% 
   map("thresholds") %>%
@@ -56,6 +57,41 @@ names(empThresh) = counts %>%
   group_by(Target) %>%
   group_keys(.) %>%
   pull("Target")
+
+####==== plot data and fitted curves
+# plotting functions
+lapsepnorm <- function(x, m, sd, guess = 0, lapse = 0){
+  guess + (1 - guess - lapse) * pnorm(x, m, sd)
+}
+
+plotfun = function(plotparameters, plotcolor){
+  curve(lapsepnorm(x, plotparameters$par$par[1], # M
+                   plotparameters$par$par[2], # SD
+                   plotparameters$par$par[3], # guess
+                   plotparameters$lapses),     # lapse
+        col = plotcolor, lwd = 2, 
+        xaxt = 'n', add=T)
+  prb <- aggregate(prob ~ prestime, FUN=mean, data=plotparameters$averages)
+  points(prb$prestime, prb$prob,
+         col=plotcolor, cex=1.5, pch = 19)
+}
+
+# actual plotting
+plot(NULL, xlim = c(-20,180), ylim = c(0,1),
+     ylab = 'p (correct)', xlab = 'presentation time (ms)', xaxt = 'n')
+plotfun(fitted2[[1]], 'red')
+plotfun(fitted2[[2]], 'blue')
+plotfun(fitted2[[3]], 'black')
+
+# add ticks, labels etc
+xvalues <- round(sort(unique(counts$prestime[counts$prestime < 500])))
+axis(1, at = xvalues)
+legend(10,0.9,legend = c('Action', 'Context', 'Object'), 
+       lty = c('solid', 'solid', 'solid'),
+       lwd = c(2,2,2),
+       col = c('red', 'blue', 'black'), bty='n')
+abline(h = 0.5, lty = 'dashed')
+
 
 ####=== randomization: generate distribution of differences between conditions 
 nruns = 1000
@@ -70,9 +106,14 @@ permThresh = rep(list(tibble(Targ1 = numeric(nruns)/0,
                         Targ1Name = char('NULL'),
                         Targ2Name = char('NULL'),
                         empDiff  = numeric(nruns)/0)),length(conditions))
-pvals = numeric(length(empThresh))/0
+#pvals = numeric(length(empThresh))/0
+pvals = tibble(D         =  rep(char('NULL'), length(empThresh)),
+               lowerTail = numeric(length(empThresh))/0,
+               upperTail = numeric(length(empThresh))/0,
+               twoSided  = numeric(length(empThresh))/0)
 
 # loop
+set.seed(12)
 for (con in 1:length(conditions)){
   for (k in 1:nruns){
   tmp = counts %>% 
@@ -102,56 +143,36 @@ for (con in 1:length(conditions)){
   permThresh[[con]][k, 6] = empThresh[conditions[[con]][1]] -
                             empThresh[conditions[[con]][2]]
   }
-  pvals[con] = sum(permThresh[[con]]$Targ1minus2 > permThresh[[con]]$empDiff[1]) / nruns
+  #pvals[con] = sum(permThresh[[con]]$Targ1minus2 > permThresh[[con]]$empDiff[1]) / nruns
+  pvals$D[con]         = paste(conditions[[con]][1], '-', conditions[[con]][2])
+  pvals$lowerTail[con] = sum(permThresh[[con]]$Targ1minus2  <=
+                          permThresh[[con]]$empDiff[1]) / nruns
+  pvals$upperTail[con] = sum(permThresh[[con]]$Targ1minus2  >=
+                          permThresh[[con]]$empDiff[1]) / nruns
+  pvals$twoSided[con]  = sum(abs(permThresh[[con]]$Targ1minus2) >=
+                          abs(permThresh[[con]]$empDiff[1])) / nruns
 }
+
 
 ####==== plot permutation distribution and empirical data
 plotPerm <- function(permResults, pvals, comparison){
-  plot(ecdf(permResults[[comparison]]$Targ1minus2), 
+  plot(ecdf(c(permResults[[comparison]]$Targ1minus2,
+              permThresh[[con]]$empDiff[1])), 
        main = paste(permResults[[comparison]]$Targ1Name[1], 
                     '-', 
-                    permResults[[comparison]]$Targ2Name[1]), 
-          xlab = "Difference of detection thresholds")
-  points(permResults[[comparison]]$empDiff[1],
-         1-pvals[comparison],
-         col = 'red')
+                    permResults[[comparison]]$Targ2Name[1]),
+              xlab = "Difference of detection thresholds")
+  #points(permResults[[comparison]]$empDiff[1],
+  #       1-pvals[comparison],
+  #       col = 'red')
+  abline(v = permResults[[comparison]]$empDiff[1],
+         col= 'red',
+         lty='dashed')
 }
 plotPerm(permThresh, pvals, 1)
 plotPerm(permThresh, pvals, 2)
 plotPerm(permThresh, pvals, 3)
 
-
-# plotting functions
-lapsepnorm <- function(x, m, sd, guess = 0, lapse = 0){
-  guess + (1 - guess - lapse) * pnorm(x, m, sd)
-}
-
-plotfun = function(plotparameters, plotcolor){
-          curve(lapsepnorm(x, plotparameters$par$par[1], # M
-                 plotparameters$par$par[2], # SD
-                 plotparameters$par$par[3], # guess
-                 plotparameters$lapses),     # lapse
-                 col = plotcolor, lwd = 2, 
-                 xaxt = 'n', add=T)
-          prb <- aggregate(prob ~ prestime, FUN=mean, data=plotparameters$averages)
-          points(prb$prestime, prb$prob,
-                 col=plotcolor, cex=1.5, pch = 19)
-}
-
-# actual plotting
-plot(NULL, xlim = c(0,180), ylim = c(0,1),
-     ylab = 'p (correct)', xlab = 'presentation time (ms)', xaxt = 'n')
-plotfun(fitted2[[1]], 'red')
-plotfun(fitted2[[2]], 'blue')
-plotfun(fitted2[[3]], 'black')
-
-# add ticks, labels etc
-xvalues <- round(sort(unique(counts$prestime[counts$prestime < 500])))
-axis(1, at = xvalues)
-legend(10,0.9,legend = c('Action', 'Context', 'Object'), 
-       lty = c('solid', 'solid', 'solid'),
-       lwd = c(2,2,2),
-       col = c('red', 'blue', 'black'), bty='n')
-abline(h = 0.5, lty = 'dashed')
+print(pvals)
 
 
